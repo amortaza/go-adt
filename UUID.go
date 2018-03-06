cff2008f3bd840b1ac0d1c5fac2e70cc12ab74ff
Afshin Mortazavi-Nia <davidnia1@gmail.com>
2018-03-05 22:02:26 -0700

    NewUUID

---
+++ UUID.go
@@ -0,0 +1,24 @@
+package adt
+
+import (
+	"io"
+	"fmt"
+	"crypto/rand"
+)
+
+func NewUUID() (string) {
+
+	uuid := make([]byte, 16)
+
+	n, err := io.ReadFull(rand.Reader, uuid)
+
+	if n != len(uuid) || err != nil {
+
+		panic("There was a problem with generating new UUID")
+	}
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:])
+}
